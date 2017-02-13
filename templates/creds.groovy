782f754dc0520bb2ec98877e36e5b9dc3b9b813b
Michael Sheinberg <mike@freedom.press>
2017-02-13 10:02:08 -0500

    Add Automation for AWS and SSH keys
    This isnt really idempotent but its a start... the way the logic works
    is that a groovy script is dropped that is run on every jenkins boot. If details
    change it isnt smart enough right now to update or delete missing keys. Will
    try to mix that logic in later. Or maybe we should be running dynamically
    against the /cli tool...... I dunno, this is good enough for now :+1:

---
+++ templates/creds.groovy
@@ -0,0 +1,40 @@
+import jenkins.model.*
+import com.cloudbees.plugins.credentials.*
+import com.cloudbees.plugins.credentials.common.*
+import com.cloudbees.plugins.credentials.domains.*
+import com.cloudbees.plugins.credentials.impl.*
+import com.cloudbees.jenkins.plugins.sshcredentials.impl.*
+import hudson.plugins.sshslaves.*;
+{% if "ec2" in jenkins_plugins -%}
+import com.cloudbees.jenkins.plugins.awscredentials.AWSCredentialsImpl
+{% endif %}
+
+domain = Domain.global()
+store = Jenkins.instance.getExtensionList('com.cloudbees.plugins.credentials.SystemCredentialsProvider')[0].getStore()
+
+{% for key in jenkins_deploy_ssh_files %}
+// SSH KEYS
+privateKey = new BasicSSHUserPrivateKey(
+    CredentialsScope.GLOBAL,
+    "{{ key.keyname }}",
+    "{{ key.username }}",
+    new BasicSSHUserPrivateKey.FileOnMasterPrivateKeySource("{{jenkins_home}}/.ssh/{{key.keyname}}"),
+    "{{ key.passphrase | default('') }}",
+    "{{ key.description | default('') }}"
+)
+store.addCredentials(domain, privateKey)
+{% endfor %}
+
+{% for key in jenkins_deploy_aws_keys %}
+// AWS KEYS
+aws_cred = new AWSCredentialsImpl(
+    CredentialsScope.GLOBAL,
+    "{{ key.id }}",
+    "{{ key.key }}",
+    "{{ key.secret }}",
+    "{{ key.desc }}",
+    "",
+    ""
+)
+store.addCredentials(domain, aws_cred)
+{% endfor %}
