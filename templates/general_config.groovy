f6bff05269b3c061f6dbc94fab2a81eb2737d1ad
Michael Sheinberg <mike@freedom.press>
2017-10-12 13:22:28 -0400

    Disable legacy node protocols
    List of protocols can be found here:
    https://github.com/jenkinsci/remoting/blob/master/docs/protocols.md

--- templates/general_config.groovy
+++ templates/general_config.groovy
@@ -1,7 +1,21 @@
 import jenkins.model.*
+import jenkins.AgentProtocol
 
 def instance = Jenkins.getInstance()
 instance.setNumExecutors({{ jenkins_deploy_config_master_exe }})
 instance.setNoUsageStatistics({{ jenkins_deploy_config_disableusage_stats | lower() }})
+
+
+// Disable deprecated Jenkins node protocols
+p = AgentProtocol.all()
+
+disable_plugin_list = ["JNLP-connect", "JNLP2-connect", "JNLP3-connect"]
+
+p.each { x ->
+	if(x.name && x.name in disable_plugin_list ) {
+      p.remove(x)
+    }
+}
+
 
 instance.save()
